5a061a5756133e2a22faf3e744880232016aeab5
joshcarver23 <joshcarver23@gmail.com>
2025-11-11 08:08:15 -0600

    Both paddles made
    adds right paddle plus some informational comments

--- import turtle.py
+++ import turtle.py
@@ -1,15 +1,27 @@
 import turtle
 
-
+#creation of the screen
 screen = turtle.Screen()
 screen.title("Pong")
 screen.bgcolor("black")
 screen.setup(width=1000, height=600)
 
+#creation of left paddle
 left_pad = turtle.Turtle()
 left_pad.shape("square")
 left_pad.color("white")
 left_pad.penup()
 left_pad.goto(-450,0)
+left_pad.shapesize(stretch_len=1,stretch_wid=5)
 
+#creation of right paddle
+right_pad = turtle.Turtle()
+right_pad.shape("square")
+right_pad.color("white")
+right_pad.penup()
+right_pad.goto(450,0)
+right_pad.shapesize(stretch_len=1,stretch_wid=5)
+
+
+#this just keeps the screen open instead of instantly closing
 screen.mainloop()
